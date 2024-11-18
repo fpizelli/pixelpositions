7f74c7e03a4cf5d58bd1e286e53d97c0e16034ce
fpizelli <168946758+fpizelli@users.noreply.github.com>
2024-11-17 21:12:18 -0300

    Finish 2/4

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,13 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
             },
             colors: {
                 "black": "#060606"
+            },
+            fontSize: {
+                "2xs": ".625rem" // 10px
             }
         },
     },
